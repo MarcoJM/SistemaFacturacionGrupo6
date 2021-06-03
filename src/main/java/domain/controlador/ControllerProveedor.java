585b3f61922f5b905c9e628a6745d9d91af594ae
MarcoJM <marcomerlos@hotmail.com>
2021-06-02 21:12:47 -0300

    .

---
+++ src/main/java/domain/controlador/ControllerProveedor.java
@@ -0,0 +1,17 @@
+package domain.controlador;
+
+public class ControllerProveedor {
+
+    public void altaProveedor(){
+
+    }
+    public void bajaProveedor(){}
+
+    public void modificarProveedor(){
+
+    }
+    public void altaDocumento(){}
+
+
+
+}
